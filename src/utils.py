c081208eeffcb840f86bbe1ed4d12491ecb086d9
Daniyar Batyrbaev <batyrbaev_daniyar@mail.ru>
2023-08-21 18:24:21 +0600

    FIX: Fix list of executed operation func

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,14 @@
 import json
 import os
+from classes import Operation
+
 
 # Получение пути к текущей директории
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Составление полного пути к файлу operations.json
 file_path = os.path.join(current_directory, '..', 'data', 'operations.json')
+
 
 def get_data(file_path):
     '''
@@ -24,29 +27,22 @@ def get_executed_operations(operations:list[dict]):
     :return: список элементов класса Operation
     '''
     operations = [op for op in operations if op and op['state'] == 'EXECUTED']
-    return operations
+    return [
+        Operation(
+            op["date"],
+            op["description"],
+            op.get("from", None),
+            op["to"],
+            op["operationAmount"]["amount"],
+            op["operationAmount"]["currency"]["name"]
+        )
+        for op in operations
+    ]
 
-def get_transaction_info(account:str):
-    '''
-    Создаем маску для счетов
-    :param account: строка с замаскированным счетом
-    :return:
-    '''
-    account_list = account.split()
-    account_name = account_list[0:-1]
-    account_number = account_list[-1]
-    if len(account_number) == 16:
-        print(f'{" ".join(account_name)} {account_number[:4]} {account_number[4:6]}** **** {account_number[-4:]}')
-    else:
-        print(f'Счет **{account_number[-4:]}')
-
-def get_sorted_operations(operations:list):
+def get_sorted_operations(operations:list[Operation]):
     '''
     Создаем функцию сортировки
     :param operations: список операций
     :return: сортированный список
     '''
     return sorted(operations, key=lambda op: op.get_datetime)
-
-
-
