e522e82bfe5ca4af611b186f1d72c1dba785ad0f
Daniyar Batyrbaev <batyrbaev_daniyar@mail.ru>
2023-08-22 16:52:29 +0600

    ADD: Add tests for test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,29 +1,30 @@
 import pytest
 
-from src.utils import get_data, get_executed_operations, get_sorted_operations
+from src.utils import file_path, get_data, get_executed_operations, get_sorted_operations
+from src.classes import Operation
 
-# Тестирование get_data
+
 def test_get_data():
     data = get_data(file_path)
     assert isinstance(data, list)
     assert len(data) > 0
-    # Дополнительные проверки по вашему выбору
 
-# Тестирование get_executed_operations
 def test_get_executed_operations():
-    data = get_data(file_path)
-    operations = get_executed_operations(data)
-    assert len(operations) > 0
-    assert all(op.get_date() is not None for op in operations)
-    assert all(op.get_description() == "Перевод организации" for op in operations)
-    # Дополнительные проверки по вашему выбору
+    sample_data = [
+        {"state": "EXECUTED", "date": "2023-08-22T10:30:00.000", "description": "Test 1", "from": "Account 1", "to": "Account 2", "operationAmount": {"amount": 100.0, "currency": {"name": "USD"}}},
+        {"state": "CANCELED", "date": "2023-08-22T11:30:00.000", "description": "Test 2", "from": "Account 2", "to": "Account 3", "operationAmount": {"amount": 50.0, "currency": {"name": "EUR"}}},
+    ]
 
-# Тестирование get_sorted_operations
+    executed_operations = get_executed_operations(sample_data)
+    assert len(executed_operations) == 1
+    assert executed_operations[0].get_description() == "Test 1"
+
 def test_get_sorted_operations():
-    data = get_data(file_path)
-    operations = get_executed_operations(data)
-    sorted_operations = get_sorted_operations(operations)
-    assert len(sorted_operations) == len(operations)
-    # Проверьте, что операции упорядочены по убыванию даты
-    assert all(sorted_operations[i].get_date() >= sorted_operations[i+1].get_date() for i in range(len(sorted_operations)-1))
-    # Дополнительные проверки по вашему выбору
+    sample_operations = [
+        Operation('2023-08-22T10:30:00.000', 'Test 1', 'Account 1', 'Account 2', 100.0, 'USD'),
+        Operation('2023-08-22T11:30:00.000', 'Test 2', 'Account 2', 'Account 3', 50.0, 'EUR'),
+    ]
+
+    sorted_operations = get_sorted_operations(sample_operations)
+    assert len(sorted_operations) == 2
+    assert sorted_operations[0].get_description() == "Test 2"
